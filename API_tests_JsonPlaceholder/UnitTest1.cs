a49e7f5e083a1f6c70bbe9d0b0465c277fe013e7
Kose3331 <148471252+Kose3331@users.noreply.github.com>
2025-02-04 18:10:47 +0200

    Add project files.

---
+++ API_tests_JsonPlaceholder/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace API_tests_JsonPlaceholder
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
